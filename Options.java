ecafdd32df7dc11e27f877daa6e6c56b1c374700
Parshya <78044438+Parshya23b@users.noreply.github.com>
2024-11-06 18:55:23 +0530

    Add files via upload

---
+++ Options.java
@@ -0,0 +1,13 @@
+package jmdevall.opencodeplan.adapter.in.commandline;
+
+import lombok.Data;
+
+@Data
+public class Options {
+	private String file;
+	private int line;
+	private int column;
+	
+	private String instruction;
+	
+}
